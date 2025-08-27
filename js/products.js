5e66bee14386ecf1feebf29f53426603536198c2
ADMIN <bannessa8a@gmail.com>
2025-08-27 15:42:32 -0600

    se agregan las funcionalidad de la lista de productos

---
+++ js/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: 1,
+        name: "Frijol de seda",
+        brand: "Don Frijol",
+        image: "./img/frijoles.webp",
+        originalPrice: "$2.00 libra",
+        discountPrice: "$1.50"
+    },
+    {
+        id: 2,
+        name: "Azucar morena",
+        brand: "Del cañal",
+        image: "./img/Azucar.webp",
+        originalPrice: "$0.80 2 libras",
+        discountPrice: "$0.65"
+    },
+    {
+        id: 3,
+        name: "Espaguettis",
+        brand: "Don chef",
+        image: "./img/spaguettis2.webp",
+        originalPrice: "$1.00 3pack",
+        discountPrice: "$0.90"
+    },
+    {
+        id: 4,
+        name: "Arroz",
+        brand: "San Francisco",
+        image: "./img/arroz.webp",
+        originalPrice: "$0.80 2 libras",
+        discountPrice: "$0.65"
+    },
+    {
+        id: 5,
+        name: "Verduras",
+        brand: "Variedad",
+        image: "./img/verduras.webp",
+        originalPrice: "$0.60 unidad",
+        discountPrice: "$0.35"
+    },
+    {
+        id: 6,
+        name: "Lacteos la Salud",
+        brand: "Variedad",
+        image: "./img/lacteos.webp",
+        originalPrice: "$2.00 unidad",
+        discountPrice: "$1.75"
+    },
+    {
+        id: 10,
+        name: "Papel Higienico",
+        brand: "Scott",
+        image: "./img/papel.webp",
+        originalPrice: "$9.99 24pck",
+        discountPrice: "$6.99"
+    },
+    {
+        id: 11,
+        name: "Articulos de limpieza",
+        brand: "Variado",
+        image: "./img/articulos de limpieza.webp",
+        originalPrice: "$3.00 unidad",
+        discountPrice: "$2.25"
+    },
+    {
+        id: 12,
+        name: "Pan",
+        brand: "Variedad",
+        image: "./img/pan.webp",
+        originalPrice: "$0.75 unidad",
+        discountPrice: "$0.50"
+    }
+];
